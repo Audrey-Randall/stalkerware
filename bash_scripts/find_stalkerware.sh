7099a280e347cdfa7cdbebc5e9ef4dadadb46752
Audrey Randall <aura6466@colorado.edu>
2019-09-25 10:21:26 -0700

    Replace find_stalkerware.sh with correct version

--- bash_scripts/find_stalkerware.sh
+++ bash_scripts/find_stalkerware.sh
@@ -9,6 +9,7 @@ starttime=$(date +%s%3N)
 OLDIFS=$IFS
 IFS=,
 domains=()
+result_file=kim_results.txt
 
 [ ! -f $filename ] && { echo "$filename file not found"; exit 99; }
 while read domain
@@ -25,14 +26,27 @@ while true; do
 		skip=$?
 		if [ "$skip" -eq "0" ]
 		then
-			echo "Servfail"
+			# echo "Servfail"
 			sleep 1.0
 			continue
 		fi
 		ms=`echo $digRes | grep -o "Query time: .* msec" | grep -o "[0-9]*"`
-		ttl=`echo $digRes | grep -o "ANSWER SECTION:\s\S*\s[0-9]*\sIN\sA" | grep -o "\s[0-9]*\s"`
-		timeElapsed=$(($(date +%s%3N)-starttime))
-		echo "Domain: " $d " TTL:" $ttl 
+		cname=`echo $digRes | grep -o "CNAME"`
+		cname=$?
+		if [ "$cname" -eq "1" ]
+		then
+			# This is an A record
+			ttl=`echo $digRes | grep -o "ANSWER SECTION:\s\S*\s[0-9]*\sIN\sA" | grep -o "\s[0-9]*\s"`
+			echo "A,"$d","$ttl","`date +%s`>>$result_file
+		else
+			# This is a CNAME record. Record both the CNAME TTL and the A record TTL.
+			cname_ttl=`echo $digRes | grep -o "ANSWER SECTION:\s\S*\s[0-9]*\sIN\sCNAME" | grep -o "\s[0-9]*\s"`	
+			ttl=`echo $digRes | grep -o "CNAME\s*\S*\s\S*\s[0-9]*\sIN\sA" | grep -o "\s[0-9]*\s"`
+			a_record=`echo $digRes | grep -o "CNAME\s*\S*\s" | grep  -o "\s\S*"`
+			echo "CNAME,"$d","$cname_ttl","`date +%s`>>$result_file
+			echo "A,"$a_record","$ttl","`date +%s`>>$result_file
+		fi
+		# timeElapsed=$(($(date +%s%3N)-starttime))
 		sleep 1.0
 	done
 done
