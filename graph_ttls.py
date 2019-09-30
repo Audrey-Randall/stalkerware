ee06958441657c31aa171ce21db2d38e8d383035
Audrey-Randall <audrey.randall@colorado.edu>
2019-09-30 10:34:14 -0700

    Add actual TTL lines to plots, bug fixes, cleanup.

--- graph_ttls.py
+++ graph_ttls.py
@@ -92,13 +92,12 @@ def calculateTTLLines(timestamps, ttls):
     
     # Figure out how many unique intercepts there are
     intercepts.sort()
+    unique_intercepts.append(intercepts[0])
     for i in range(0, len(intercepts)-1):
         if intercepts[i] != intercepts[i+1]:
-            unique_intercepts.append(intercepts[i])
-    if intercepts[-1] != intercepts[-2]:
-        unique_intercepts.append(intercepts[-1])
+            unique_intercepts.append(intercepts[i+1])
     print("Total intercepts: " + str(len(intercepts)))
-    return unique_intercepts
+    return np.array(unique_intercepts)
 
 def getPointRange(start_time, end_time, ts):
     # Take the first index where the timestamp is larger than the start time,
@@ -134,33 +133,35 @@ def sortByDomain(ttls_by_domain, ts_by_domain, max_ttls_by_domain):
         # to see the highest TTL. 
         max_ttls_by_domain[d] = max_ttls_by_domain[d]+ 1
 
-def plotReplicasOverTime(domain, unique_intercepts, start_time, end_time, max_ttl):
+def plotReplicasOverTime(domain, unique_intercepts, start_time, end_time, max_ttl, scale_ttls=False):
     # Given the set of y-intercepts of all the TTL lines, plot the number of replicas 
     # active at a time.
     lowest_intercept_idx = 0
     active_replicas = []
-    for t in np.arange(start_time, end_time):
+    time_intervals = np.arange(start_time, end_time, max_ttl)
+    np_intercepts = np.array(unique_intercepts)
+    ttl_scaling_factor = 1.0
+    if scale_ttls:
+        ttl_scaling_factor = 60.0 / float(max_ttl)
+    # print(time_intervals)
+    for t in time_intervals:
         # Let y_int be the y-intercept of the TTL line.
         # The relevant TTL lines at time t will have intercepts between y=t and y=t + max_ttl,
         # because the slope is -1.
         low_bound = t
         high_bound = t + max_ttl
 
-        while unique_intercepts[lowest_intercept_idx] < t:
-            lowest_intercept_idx += 1
+        # Get the intercepts that are between the bounds
+        active_ttl_line_idxs = np.where(np.logical_and(np_intercepts > low_bound, np_intercepts < high_bound))[0]
+        active_replicas.append(len(active_ttl_line_idxs) * ttl_scaling_factor)
 
-        intercept_idx = lowest_intercept_idx
-        num_active_ttl_lines = 0
-        # Find all intercepts in unique_intercept that are between low_bound and high_bound
-        while unique_intercepts[intercept_idx] < high_bound:
-            num_active_ttl_lines += 1
-            intercept_idx += 1
-
-        active_replicas.append(num_active_ttl_lines)
-
-    plt.plot(np.arange(start_time, end_time), active_replicas)
-    figname = 'kim_results/active_replicas/' + domain + '_active_replicas.png'
-    xlabel = "Timestamp. (TTL = " + str(max_ttl) + ")"
+    plt.plot(time_intervals, active_replicas)
+    figname = 'kim_results/test_active_replicas/' + domain
+    if scale_ttls:
+        figname += '_adjusted_active_replicas.png'
+    else:
+        figname += '_active_replicas.png'
+    xlabel = "Timestamp (TTL = " + str(max_ttl) + ")"
     plt.xlabel(xlabel)
     plt.ylabel("Number of active replicas")
     title = "Active replicas for " + domain
@@ -181,30 +182,74 @@ def performAnalysis():
     sortByDomain(ttls_by_domain, ts_by_domain, max_ttls_by_domain)
 
     for d in ts_by_domain:
+        # Normalize the timestamps so they start at 0. Also normalize the intercepts.
+        first_ts = ts_by_domain[d][0]
+        np_ts = np.array(ts_by_domain[d]) - first_ts
+        np_ttls = np.array(ttls_by_domain[d])
+
         # Determine how many unique TTL lines exist.
-        unique_intercepts = calculateTTLLines(np.array(ts_by_domain[d]), np.array(ttls_by_domain[d]))
+        unique_intercepts = calculateTTLLines(np_ts, np_ttls)
         print("Unique intercepts for " + d + ": " + str(len(unique_intercepts)))
 
         max_ttl = max_ttls_by_domain[d]
         start_time = 0
         # end_time = 20 * max_ttl + start_time
-        end_time = 60 * 60 * 24
-        plotReplicasOverTime(d, unique_intercepts, start_time, end_time, max_ttl)
+        end_time = max_ttl
 
-        # # Normalize the timestamps so they start at 0
-        # np_ts = np.array(ts_by_domain[d])
-        # np_ts = np_ts - np_ts[0]
+        if d == "amplitude.life360.com":
+            plotReplicasOverTime(d, unique_intercepts, start_time, end_time, max_ttl)
 
-        # # Determine what range of points to display to easily see the TTL lines.
-        # idx_range = getPointRange(start_time, end_time, np_ts)
+            # Determine what range of points to display to easily see the TTL lines.
+            idx_range = getPointRange(start_time, end_time, np_ts)
 
-        # # Get the chunks of the arrays to plot, and plot them.
-        # ts_in_range = np_ts[idx_range]
-        # ttls_in_range = np.array(ttls_by_domain[d])[idx_range]
-        # plotTsVsTTLs(ts_in_range, ttls_in_range, start_time, end_time, max_ttl, d)
+            # Get the chunks of the arrays to plot, and plot them.
+            ts_in_range = np_ts[idx_range]
+            ttls_in_range = np.array(ttls_by_domain[d])[idx_range]
+            np_intercepts_in_range = unique_intercepts[np.where(np.logical_and(unique_intercepts < 2 * end_time, unique_intercepts > start_time))[0]]
+            print("np_intercepts: ", np_intercepts_in_range)
+            for i in range(0, len(ts_in_range)):
+                print(ts_in_range[i], ttls_in_range[i])
+            plotTsVsTTLs(ts_in_range, ttls_in_range, start_time, end_time, max_ttl, d, np_intercepts_in_range)
 
+def plotTsVsTTLs(ts, ttls, start, end, max_ttl, domain, unique_intercepts=[]):
+    for i in unique_intercepts:
+        plt.plot([i - max_ttl, i], [max_ttl, 0], color="orange", linewidth=0.5)
+    plt.plot(ts - start, ttls, linestyle="",marker="o", markersize=2.0)
+    plt.grid(axis='x', linewidth=0.5, linestyle = 'dashed', which='minor')
+    plt.xticks(np.arange(0, end-start, max_ttl))
+    plt.axes().xaxis.set_major_locator(plt.MaxNLocator(10))
+    plt.axes().xaxis.set_minor_locator(plt.MaxNLocator(20))
+    plt.xlim(start, end)
+    plt.ylim(0, max_ttl)
+    xlabel = 'Timestamp (seconds after ' + str(start) +'s)'
+    plt.xlabel(xlabel)
+    plt.ylabel('TTL (seconds)')
+    title = 'TTL lines for ' + domain
+    plt.title(title)
+    figname = 'kim_results/kim_result_graphs/' + domain + '_ttls.png'
+    plt.savefig(figname)
+    plt.show()
+    plt.close()
 
+def plotRespTimeVsTTLs():
+    plt.plot(query_results['response_time'], query_results[' ttl'], linestyle="",marker="o", markersize=2.0)
+    plt.xlabel('Response time (ms)')
+    plt.ylabel('TTL (seconds)')
+    title = 'TTLs vs response times: query for facebook.com to 8.8.8.8,\n recursion desired'
+    plt.title(title)
+    figname = 'resp_time_vs_ttl_facebookcom.png'
+    plt.savefig(figname)
+    plt.show()
 
+def plotWallTimeVsTTLs():
+    plt.plot(query_results[' time_elapsed'], query_results[' ttl'], linestyle="",marker="o", markersize=2.0)
+    plt.xlabel('Time elapsed (seconds)')
+    plt.ylabel('TTL (seconds)')
+    title = 'TTLs vs time elapsed: query for facebook.com to 8.8.8.8,\n recursion desired'
+    plt.title(title)
+    #figname = 'wall_time_vs_ttl_facebookcom.png'
+    #plt.savefig(figname)
+    plt.show()
 
 '''
 Assumes the csv looks like this:
@@ -241,54 +286,18 @@ def readDigResults(filename):
                 query_results[colName].append(int(item))
 
 
-def plotTsVsTTLs(start, end, filename):
-    plt.plot(query_results['ts'][start:end], query_results['ttls'][start:end], linestyle="",marker="o", markersize=2.0)
-    plt.grid(axis='x', linewidth=0.5, linestyle = 'dashed', which='minor')
-    #plt.xticks(np.arange(start, end, 300))
-    plt.xticks(np.arange(int(query_results['ts'][start]), int(query_results['ts'][end]), end-start), which='minor')
-    plt.xlabel('Timestamp (seconds since first timestamp)')
-    plt.ylabel('TTL (seconds)')
-    title = 'TTL lines for ' + str(int(query_results['ts'][start])) + ' seconds to ' + str(int(query_results['ts'][end])) + ' seconds'
-    plt.title(title)
-    figname = filename.split('.')[0] + '_' + str(start) + '_' + str(end) + '.png'
-    plt.savefig(figname)
-    plt.show()
-
-def plotTsVsTTLs(ts, ttls, start, end, max_ttl, domain):
-    plt.plot(ts - start, ttls, linestyle="",marker="o", markersize=2.0)
-    plt.grid(axis='x', linewidth=0.5, linestyle = 'dashed', which='minor')
-    plt.xticks(np.arange(0, end-start, max_ttl))
-    plt.axes().xaxis.set_major_locator(plt.MaxNLocator(10))
-    plt.axes().xaxis.set_minor_locator(plt.MaxNLocator(20))
-    xlabel = 'Timestamp (seconds after ' + str(start) +'s)'
-    plt.xlabel(xlabel)
-    plt.ylabel('TTL (seconds)')
-    title = 'TTL lines for ' + domain
-    plt.title(title)
-    figname = 'kim_results/kim_result_graphs/' + domain + '_ttls.png'
-    plt.savefig(figname)
-    plt.show()
-    plt.close()
-
-def plotRespTimeVsTTLs():
-    plt.plot(query_results['response_time'], query_results[' ttl'], linestyle="",marker="o", markersize=2.0)
-    plt.xlabel('Response time (ms)')
-    plt.ylabel('TTL (seconds)')
-    title = 'TTLs vs response times: query for facebook.com to 8.8.8.8,\n recursion desired'
-    plt.title(title)
-    figname = 'resp_time_vs_ttl_facebookcom.png'
-    plt.savefig(figname)
-    plt.show()
-
-def plotWallTimeVsTTLs():
-    plt.plot(query_results[' time_elapsed'], query_results[' ttl'], linestyle="",marker="o", markersize=2.0)
-    plt.xlabel('Time elapsed (seconds)')
-    plt.ylabel('TTL (seconds)')
-    title = 'TTLs vs time elapsed: query for facebook.com to 8.8.8.8,\n recursion desired'
-    plt.title(title)
-    #figname = 'wall_time_vs_ttl_facebookcom.png'
-    #plt.savefig(figname)
-    plt.show()
+# def plotTsVsTTLs(start, end, filename):
+#     plt.plot(query_results['ts'][start:end], query_results['ttls'][start:end], linestyle="",marker="o", markersize=2.0)
+#     plt.grid(axis='x', linewidth=0.5, linestyle = 'dashed', which='minor')
+#     #plt.xticks(np.arange(start, end, 300))
+#     plt.xticks(np.arange(int(query_results['ts'][start]), int(query_results['ts'][end]), end-start), which='minor')
+#     plt.xlabel('Timestamp (seconds since first timestamp)')
+#     plt.ylabel('TTL (seconds)')
+#     title = 'TTL lines for ' + str(int(query_results['ts'][start])) + ' seconds to ' + str(int(query_results['ts'][end])) + ' seconds'
+#     plt.title(title)
+#     figname = filename.split('.')[0] + '_' + str(start) + '_' + str(end) + '.png'
+#     plt.savefig(figname)
+#     plt.show()
 
 def makeHiveGraph(filename):
     readHiveResultFile()
@@ -303,3 +312,10 @@ def makeDigGraph(filename):
 # filename = "bash_scripts/timing_attack_rate_limited.csv"
 # makeDigGraph(filename)
 performAnalysis()
+# xs = [1,2,3,4,5,6,7,8,9,10]
+# ys = [5,4,3,2,1,3,2,1,5,4]
+# unique_intercepts = calculateTTLLines(xs, ys)
+# print(unique_intercepts)
+# plt.plot(xs, ys, linestyle="",marker="o", markersize=2.0)
+# plt.savefig("kim_results/test_active_replicas/test.png")
+# plt.show()
